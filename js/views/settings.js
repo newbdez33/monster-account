92ca8ef021f996a1a9933925fb60c166319bdc14
Jacky <newbdez33@gmail.com>
2013-11-05 21:14:17 +0800

    imported data

--- js/views/settings.js
+++ js/views/settings.js
@@ -3,6 +3,10 @@ app.SettingsView = Backbone.View.extend({
 
     template: _.template($('#settings-template').html()),
 
+    events: {
+        "click #importbutton": "importAction",
+    },
+
     initialize: function (options) {
     	//
     },
@@ -11,6 +15,30 @@ app.SettingsView = Backbone.View.extend({
 
         this.$el.html(this.template({currentUser:app.currentUser}));
         return this;
+    },
+
+    importAction: function() {
+        var o = JSON.parse($('#importdata').val());
+        _.each(o, function(item){
+            
+            var spend = new app.Spend();
+            spend.set("category", item.category);
+            spend.set("date", item.spendDatetime);
+            spend.set("memo", item.memo);
+            spend.set("spend", parseInt(item.number));
+            spend.set("user", app.currentUser);
+            spend.setACL(new Parse.ACL(app.currentUser));
+            
+            spend.save(null, {
+                success: function(obj) {
+                    console.log("")
+                },
+                error: function(obj, error) {
+                    console.log(error)
+                }
+            });
+
+        });
     },
 
 });
